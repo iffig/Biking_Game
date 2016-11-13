b99f05bc994686b810c1fbb4565be336e0a08a05
Brandon <bbarrett@victorops.com>
2016-11-13 13:33:02 -0700

    Revert "Revert "UC2: Pause Game ""

---
+++ src/com/csciOOD/Menu.java
@@ -0,0 +1,11 @@
+package com.csciOOD;
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import com.csciOOD.Screen;
+
+public class Menu extends JPanel {
+    private boolean isPaused = false;
+
+    public Menu(){}
+
+}
