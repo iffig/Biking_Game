f09f922097b32c1530ea5dfbff46eb73aca8a301
iffig <isabella.figueroa@colorado.edu>
2016-11-30 13:34:12 -0700

    Removed Debugging notes

--- src/com/csciOOD/ObstacleFactory.java
+++ src/com/csciOOD/ObstacleFactory.java
@@ -10,20 +10,16 @@ public class ObstacleFactory {
         int obstChoice = randomObst.nextInt(4);
 
         if(obstChoice == 0){
-            System.out.print("Frog");
             return new Frog();
         }
 
         else if(obstChoice == 1) {
-            System.out.print("butter");
             return new Butterfly();
         }
         else if(obstChoice == 2) {
-            System.out.print("bird");
             return new Bird();
         }
         else if(obstChoice == 3) {
-            System.out.print("biker");
             return new Biker();
         }
 
