7f65f9d87850d2d9689805181eced23be4a71e91
iffig <isabella.figueroa@colorado.edu>
2016-12-03 13:24:39 -0700

    Fixed score bugs i think

--- src/com/csciOOD/ScoreTracker.java
+++ src/com/csciOOD/ScoreTracker.java
@@ -15,11 +15,11 @@ public class ScoreTracker implements Tracker {
 
     @Override
     public void decrement(int pointValue) {
-        if ((score - pointValue) <= 0){
-            score = 0;
-        }
-        else{
+        if(score - pointValue >= 0){
             score -= pointValue;
+        }
+        else if(score - pointValue < 0){
+            score = 0;
         }
     }
 
@@ -27,7 +27,7 @@ public class ScoreTracker implements Tracker {
     public void painting(Graphics g) {
         g.setColor(Color.black);
         g.drawString("Score ", 50, 45);
-        g.drawString(( Integer.toString(score)), 100, 45);
+        g.drawString((Integer.toString(score)), 100, 45);
 
     }
 }
